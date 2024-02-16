e61296a5ce772d7aea7afdbcd1198fb2bc1a839f
St-Daniel-H <danihujairy66@gmail.com>
2024-02-16 19:33:27 +0200

    quick update2

--- src/SCSS/cursorVariable.ts
+++ src/SCSS/cursorVariable.ts
@@ -1,8 +1,8 @@
 const cursor = {
-  defaultCursor: "../../dist/public/Images/Cursor/cursorDefault.cur",
-  aimCursor: "../../dist/public/Images/Cursor/cursorAim.cur",
-  fireCursor: "../../dist/public/Images/Cursor/cursorShoot.png",
-  inputCursor:"../../dist/public/Images/Cursor/cursorInput.cur",
+  defaultCursor: "../../dist/Images/Cursor/cursorDefault.cur",
+  aimCursor: "../../dist/Images/Cursor/cursorAim.cur",
+  fireCursor: "../../dist/Images/Cursor/cursorShoot.png",
+  inputCursor:"../../dist/Images/Cursor/cursorInput.cur",
   //not used yet below,
   noammoCursor: "http://www.rw-designer.com/cursor-extern.php?id=69740",
   reloadCursor: "http: //www.rw-designer.com/cursor-extern.php?id=69759",
